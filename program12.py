2adc41f7335487ec58aba7316f8da3c5f1ec779c
Smoki <64897895+Smoki-Cool@users.noreply.github.com>
2023-01-03 11:33:13 +0530

    finished program12

---
+++ program12.py
@@ -0,0 +1,24 @@
+'''
+Input a list of numbers and swap elements at the even location with the elements at the odd location.
+'''
+
+inp: list = eval(input("\nEnter your list of numbers: "))
+if type(inp) == list:
+    cond = True
+    
+    for i in inp:
+        if type(i) == int or type(i) == float:
+            pass
+        else:
+            print("\nERROR: All elements of the list must be a number.\n")
+            cond = False
+            break
+    
+    if cond:
+        for i in range(0, len(inp), 2):
+            inp.insert(i + 1, inp.pop(i))
+            
+        print(inp)
+        
+else:
+    print("\nERROR: Please enter a list only.\n")
