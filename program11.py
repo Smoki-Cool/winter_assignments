32ee60b2d8840f0ebf56372a0937d3ef697d5661
Smoki <64897895+Smoki-Cool@users.noreply.github.com>
2023-01-06 11:05:13 +0530

    rewrote program11 with logic

--- program11.py
+++ program11.py
@@ -4,7 +4,27 @@ Find the largest/smallest number in a list/tuple.
 
 inp = eval(input("\nEnter your list/tuple: "))
 if type(inp) == list or type(inp) == tuple:
-    print(f'\nLargest: {max(inp)}\nSmallest: {min(inp)}\n')
+    cond = True
+    
+    for i in inp:
+        if type(i) == int or type(i) == float:
+            pass
+        else:
+            print("\nERROR: All elements of the list must be a number.\n")
+            cond = False
+            break
+    
+    largest = inp[0]
+    smallest = inp[0]
+    
+    if cond:
+        for i in inp[1:]:
+            if i > largest:
+                largest = i
+            elif i < smallest:
+                smallest = i
+            
+        print(f'\nLargest Number: {largest}\nSmallest Number: {smallest}\n')
 
 else:
     print("\nERROR: Please enter a list/tuple only.\n")
