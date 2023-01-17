0cff63ac150c991d8e2be1b8b4cc7bbbdf74184c
Smoki <64897895+Smoki-Cool@users.noreply.github.com>
2023-01-17 16:49:51 +0530

    finished program14

---
+++ program14.py
@@ -0,0 +1,30 @@
+'''
+Input a list of numbers and find the smallest and largest number from the list.
+'''
+
+inp: list = eval(input("\nEnter your list of numbers: "))
+if type(inp) == list:
+    cond = True
+    
+    for i in inp:
+        if type(i) == int or type(i) == float:
+            pass
+        else:
+            print("\nERROR: All elements of the list must be a number.\n")
+            cond = False
+            break
+    
+    largest = inp[0]
+    smallest = inp[0]
+    
+    if cond:
+        for i in inp[1:]:
+            if i > largest:
+                largest = i
+            elif i < smallest:
+                smallest = i
+            
+        print(f'\nLargest Number: {largest}\nSmallest Number: {smallest}\n')
+        
+else:
+    print("\nERROR: Please enter a list only.\n")
